243a003c74942d896ac2da6365fd5471cc489ea3
tharinduEranga <tharindueranga3@gmail.com>
2021-09-26 22:08:04 +0530

    update similar symptoms UI screen

--- best_flutter_ui_templates/lib/introduction_animation/components/care_view.dart
+++ best_flutter_ui_templates/lib/introduction_animation/components/care_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../introduction_animation_screen.dart';
 
-class CareView extends StatelessWidget {
+class CareView extends StatefulWidget {
   final List<SymptomDTO> selectedSymptoms;
 
   final AnimationController animationController;
@@ -14,11 +14,16 @@ class CareView extends StatelessWidget {
       : super(key: key);
 
   @override
+  _CareViewState createState() => _CareViewState();
+}
+
+class _CareViewState extends State<CareView> {
+  @override
   Widget build(BuildContext context) {
     final _firstHalfAnimation =
         Tween<Offset>(begin: Offset(1, 0), end: Offset(0, 0))
             .animate(CurvedAnimation(
-      parent: animationController,
+      parent: widget.animationController,
       curve: Interval(
         0.2,
         0.4,
@@ -28,7 +33,7 @@ class CareView extends StatelessWidget {
     final _secondHalfAnimation =
     Tween<Offset>(begin: Offset(0, 0), end: Offset(-1, 0))
         .animate(CurvedAnimation(
-      parent: animationController,
+      parent: widget.animationController,
       curve: Interval(
         0.4,
         0.6,
@@ -37,39 +42,42 @@ class CareView extends StatelessWidget {
     ));
     final _relaxFirstHalfAnimation =
     Tween<Offset>(begin: Offset(2, 0), end: Offset(0, 0))
-        .animate(CurvedAnimation(
-      parent: animationController,
+            .animate(CurvedAnimation(
+      parent: widget.animationController,
       curve: Interval(
         0.2,
         0.4,
-        curve: Curves.fastOutSlowIn,
-      ),
-    ));
-    final _relaxSecondHalfAnimation =
-    Tween<Offset>(begin: Offset(0, 0), end: Offset(-2, 0))
-        .animate(CurvedAnimation(
-      parent: animationController,
-      curve: Interval(
-        0.4,
-        0.6,
         curve: Curves.fastOutSlowIn,
       ),
     ));
 
+    final _relaxSecondHalfAnimation =
+        Tween<Offset>(begin: Offset(0, -2), end: Offset(0, 0)).animate(
+      CurvedAnimation(
+        parent: widget.animationController,
+        curve: Interval(
+          0.0,
+          0.2,
+          curve: Curves.fastOutSlowIn,
+        ),
+      ),
+    );
+
     final _imageFirstHalfAnimation =
-    Tween<Offset>(begin: Offset(4, 0), end: Offset(0, 0))
-        .animate(CurvedAnimation(
-      parent: animationController,
+        Tween<Offset>(begin: Offset(4, 0), end: Offset(0, 0))
+            .animate(CurvedAnimation(
+      parent: widget.animationController,
       curve: Interval(
         0.2,
         0.4,
         curve: Curves.fastOutSlowIn,
       ),
     ));
-    final _imageSecondHalfAnimation =
-    Tween<Offset>(begin: Offset(0, 0), end: Offset(-4, 0))
-        .animate(CurvedAnimation(
-      parent: animationController,
+
+    final _imageAnimation =
+        Tween<Offset>(begin: Offset(0, 0), end: Offset(-4, 0))
+            .animate(CurvedAnimation(
+      parent: widget.animationController,
       curve: Interval(
         0.4,
         0.6,
@@ -82,22 +90,13 @@ class CareView extends StatelessWidget {
       child: SlideTransition(
         position: _secondHalfAnimation,
         child: Padding(
-          padding: const EdgeInsets.only(bottom: 100),
+          padding: const EdgeInsets.all(20),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              SlideTransition(
-                position: _imageFirstHalfAnimation,
-                child: SlideTransition(
-                  position: _imageSecondHalfAnimation,
-                  child: Container(
-                    constraints: BoxConstraints(maxWidth: 350, maxHeight: 250),
-                    child: Image.asset(
-                      'assets/introduction_animation/care_image.png',
-                      fit: BoxFit.contain,
-                    ),
-                  ),
-                ),
+              /*top padding*/
+              Padding(
+                padding: EdgeInsets.all(50.0),
               ),
               SlideTransition(
                 position: _relaxFirstHalfAnimation,
@@ -110,14 +109,45 @@ class CareView extends StatelessWidget {
                   ),
                 ),
               ),
-              Padding(
-                padding:
-                EdgeInsets.only(left: 64, right: 64, bottom: 16, top: 16),
-                child: Text(
-                  selectedSymptoms.toString(),
-                  textAlign: TextAlign.center,
-                ),
+              SlideTransition(
+                  position: _imageAnimation,
+                  child: Column(children: <Widget>[
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                  ])),
+              SizedBox(height: 150), // give it height
+              Column(
+                children: [
+                  SlideTransition(
+                    position: _relaxSecondHalfAnimation,
+                    child: Text(
+                      "Selected symptoms",
+                      style: TextStyle(
+                          fontSize: 16.0, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
               ),
+              ConstrainedBox(
+                  constraints: new BoxConstraints(
+                    minHeight: 35.0,
+                    maxHeight: 165.0,
+                  ),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      // if you need this
+                      side: BorderSide(
+                        color: Colors.grey.withOpacity(0.2),
+                        width: 1,
+                      ),
+                    ),
+                    child: ListView(
+                      padding: const EdgeInsets.all(8),
+                      children: [],
+                    ),
+                  )),
             ],
           ),
         ),
