9f1375fb392aab70550958ff1875196f7e3e92e0
tharinduEranga <tharindueranga3@gmail.com>
2021-10-03 16:11:18 +0530

    add matched disease UI widget only

--- best_flutter_ui_templates/lib/introduction_animation/components/mood_diary_vew.dart
+++ best_flutter_ui_templates/lib/introduction_animation/components/mood_diary_vew.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 
-class MoodDiaryVew extends StatelessWidget {
+class MoodDiaryVew extends StatefulWidget {
   final AnimationController animationController;
 
   const MoodDiaryVew({Key? key, required this.animationController})
       : super(key: key);
 
   @override
+  _MoodDiaryVewState createState() => _MoodDiaryVewState();
+}
+
+class _MoodDiaryVewState extends State<MoodDiaryVew> {
+  @override
   Widget build(BuildContext context) {
     final _firstHalfAnimation =
         Tween<Offset>(begin: Offset(1, 0), end: Offset(0, 0))
             .animate(CurvedAnimation(
-      parent: animationController,
+      parent: widget.animationController,
       curve: Interval(
         0.4,
         0.6,
@@ -21,7 +26,7 @@ class MoodDiaryVew extends StatelessWidget {
     final _secondHalfAnimation =
         Tween<Offset>(begin: Offset(0, 0), end: Offset(-1, 0))
             .animate(CurvedAnimation(
-      parent: animationController,
+      parent: widget.animationController,
       curve: Interval(
         0.6,
         0.8,
@@ -32,37 +37,41 @@ class MoodDiaryVew extends StatelessWidget {
     final _moodFirstHalfAnimation =
         Tween<Offset>(begin: Offset(2, 0), end: Offset(0, 0))
             .animate(CurvedAnimation(
-      parent: animationController,
+      parent: widget.animationController,
       curve: Interval(
+        0.2,
         0.4,
-        0.6,
         curve: Curves.fastOutSlowIn,
       ),
     ));
+
     final _moodSecondHalfAnimation =
-        Tween<Offset>(begin: Offset(0, 0), end: Offset(-2, 0))
-            .animate(CurvedAnimation(
-      parent: animationController,
-      curve: Interval(
-        0.6,
-        0.8,
-        curve: Curves.fastOutSlowIn,
+        Tween<Offset>(begin: Offset(0, -2), end: Offset(0, 0)).animate(
+      CurvedAnimation(
+        parent: widget.animationController,
+        curve: Interval(
+          0.0,
+          0.2,
+          curve: Curves.fastOutSlowIn,
+        ),
       ),
-    ));
+    );
+
     final _imageFirstHalfAnimation =
         Tween<Offset>(begin: Offset(4, 0), end: Offset(0, 0))
             .animate(CurvedAnimation(
-      parent: animationController,
+      parent: widget.animationController,
       curve: Interval(
         0.4,
         0.6,
         curve: Curves.fastOutSlowIn,
       ),
     ));
+
     final _imageSecondHalfAnimation =
         Tween<Offset>(begin: Offset(0, 0), end: Offset(-4, 0))
             .animate(CurvedAnimation(
-      parent: animationController,
+      parent: widget.animationController,
       curve: Interval(
         0.6,
         0.8,
@@ -75,41 +84,57 @@ class MoodDiaryVew extends StatelessWidget {
       child: SlideTransition(
         position: _secondHalfAnimation,
         child: Padding(
-          padding: const EdgeInsets.only(bottom: 100),
+          padding: const EdgeInsets.all(20),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Text(
-                "Mood Dairy",
-                style: TextStyle(fontSize: 26.0, fontWeight: FontWeight.bold),
+              /*top padding*/
+              Padding(
+                padding: EdgeInsets.all(50.0),
               ),
               SlideTransition(
                 position: _moodFirstHalfAnimation,
                 child: SlideTransition(
                   position: _moodSecondHalfAnimation,
-                  child: Padding(
-                    padding: EdgeInsets.only(
-                        left: 64, right: 64, top: 16, bottom: 16),
+                  child: Text(
+                    "Matched disease",
+                    style:
+                        TextStyle(fontSize: 26.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              SizedBox(height: 50),
+              Column(
+                children: [
+                  SlideTransition(
+                    position: _moodSecondHalfAnimation,
                     child: Text(
-                      "Lorem ipsum dolor sit amet,consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore",
-                      textAlign: TextAlign.center,
+                      "Selected symptoms",
+                      style: TextStyle(
+                          fontSize: 16.0, fontWeight: FontWeight.bold),
                     ),
                   ),
-                ),
+                ],
               ),
-              SlideTransition(
-                position: _imageFirstHalfAnimation,
-                child: SlideTransition(
-                  position: _imageSecondHalfAnimation,
-                  child: Container(
-                    constraints: BoxConstraints(maxWidth: 350, maxHeight: 250),
-                    child: Image.asset(
-                      'assets/introduction_animation/mood_dairy_image.png',
-                      fit: BoxFit.contain,
-                    ),
+              ConstrainedBox(
+                  constraints: new BoxConstraints(
+                    minHeight: 35.0,
+                    maxHeight: 165.0,
                   ),
-                ),
-              ),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      // if you need this
+                      side: BorderSide(
+                        color: Colors.grey.withOpacity(0.2),
+                        width: 1,
+                      ),
+                    ),
+                    child: ListView(
+                      padding: const EdgeInsets.all(8),
+                      children: [],
+                    ),
+                  )),
             ],
           ),
         ),
